490554cfe4fdb0fec7f5be422ace0a1a44e3f8ce
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2025-12-23 09:16:21 +0200

    feat: Argument parsing

--- Sources/fastcards/main.swift
+++ Sources/fastcards/main.swift
@@ -16,3 +16,21 @@ do {
 }
 
 // MARK: Entrypoint
+let args = CommandLine.arguments
+
+if args.count > 1 {
+    let action = args[1]
+
+    switch action {
+        case "list":
+            let decks = DeckStore.listDecks()
+            if decks.isEmpty {
+                print("󰘸 No decks found")
+            } else {
+                decks.forEach{ print($0) }
+            }
+        default:
+            print("Usage: fastcards <action> <arguments>")
+            print("> list - Lists available decks")
+    }
+}
