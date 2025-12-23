a17ad2cd11776c853e7765c61d723f6264f36515
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2025-12-23 09:21:31 +0200

    feat: Deck creation

--- Sources/fastcards/main.swift
+++ Sources/fastcards/main.swift
@@ -29,6 +29,27 @@ if args.count > 1 {
             } else {
                 decks.forEach{ print($0) }
             }
+        case "createdeck":
+            guard args.count > 2 else {
+                print("Usage: fastcards createdeck <name>")
+                exit(1)
+            }
+
+            let name = args[2]
+
+            if DeckStore.listDecks().contains(name) {
+                print("󰘸 Deck \(name) already exists!")
+                exit(1)
+            }
+            
+            let deck = Deck(name: name)
+
+            do {
+                try DeckStore.save(deck: deck)
+                print("󰘸 Deck created: \(name)")
+            } catch {
+                print(" Failed to create deck: \(error)")
+            }
         default:
             print("Usage: fastcards <action> <arguments>")
             print("> list - Lists available decks")
